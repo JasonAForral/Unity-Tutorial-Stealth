dd3e000cb069fb4bcd8193c15828d6371be59e0c
Tsinoyboi <ajsnoface@gmail.com>
2015-04-23 13:00:47 -0700

    Added MiniMap
    it looks ahead with lerp.

---
+++ Assets/MiniMapController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapController : MonoBehaviour {
+
+    public Transform focus;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Awake () {
+        offset = transform.position - focus.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //transform.position = focus.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, focus.position + offset + focus.forward * 5, 0.1f);
+	}
+}
